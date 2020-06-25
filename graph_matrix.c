a552bca4bb8f05f164b97c5b0dd45d55d2f414fc
khiem2105.1@gmail.com <khiem2105.1@gmail.com>
2020-06-25 21:33:36 +0700

    modify

--- graph_matrix.c
+++ graph_matrix.c
@@ -1,8 +1,20 @@
 #include "graph_matrix.h"
 
 int main() {
-    int V = 6;
+    int V = 6,i,j,length;
     matG *g = createGraph(V);
-    addEdge(g);
+    for(i = 0; i < V; i++) {
+        for(j = 0; j < V; j++) {
+            printf("Length %d->%d:",i,j);
+            scanf("%d",&length);
+            addEdgeDirGraph(g,i,j,length);
+        }
+    }
+    int mark[V];
+    for(i = 0; i < V; i++)
+        mark[i] = 0;
+    int pre[V];    
     printGraph(*g);
+    //profondeur(*g,0,mark,pre);
+    dijkstra(*g,0);
 }    
--- graph_matrix.h
+++ graph_matrix.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "file_list.h"
+//Adjacency matrix
 struct matG {
     int numvertices;
     int **graph;
@@ -17,22 +18,16 @@ matG* createGraph(int n) {
     }
     return g;
 }
-void addEdge(matG *g) {
-    int i = 0, j = 0, isConnected,length;
-    for(i = 0; i < g->numvertices; i++) {
-        for(j = 0; j < g->numvertices; j++) {
-            printf("Is [%d] connected to [%d]?(0 for NO, 1 for YES)\n",i,j);
-            scanf("%d",&isConnected);
-            if(isConnected) {
-                printf("Length:");
-                scanf("%d",&length);
-                g->graph[i][j] = length;
-            }
-            else
-                g->graph[i][j] = 1000;
-        }
-    }
+//Add edege dir graph
+void addEdgeDirGraph(matG *g, int source, int dest, int length) {
+    g->graph[source][dest] = length;
 }
+//Add edge undir graph
+void addEdgeUndirGraph(matG *g, int source, int dest, int length) {
+    g->graph[source][dest] = length;
+    g->graph[dest][source] = length;   
+}
+//Printf graph
 void printGraph(matG g) {
     int i = 0,j = 0;
     for(i = 0; i < g.numvertices; i++) {
@@ -42,8 +37,85 @@ void printGraph(matG g) {
         printf("\n");
     }
 }
-void profondeur(matG g, int k, int mark[]) {
-    if(mark[k] != 1) {
-        
+//Duyet sau
+void profondeur(matG g,int k,int mark[],int pre[]) {
+    mark[k] = 1;
+    printf("%d-",k);
+    for(int i = 0; i < g.numvertices; i++) {
+        if(mark[i] == 0 && g.graph[k][i] != 1000) {
+            pre[i] = k;
+            profondeur(g,i,mark,pre);
+        }
     }
+}
+//Duyet rong
+void larger(matG g, int k, int mark[], int pre[]) {
+    mark[k] = 1;
+    file_list q;
+    initFile(&q);
+    ajouteFile(&q,k);
+    while(!fileVide(q)) {
+        int current = retirerFile(&q);
+        printf("%d-",current);
+        for(int i = 0; i < g.numvertices; i++) {
+            if(mark[i] == 0 && g.graph[current][i] != 1000) {
+                mark[i] = 1;
+                ajouteFile(&q,i);
+                pre[i] = current;
+            }
+        }
+    }
+}
+//Dijkstra
+//Tim duong di nho nhat
+int minDistance(int distance[],int visited[], int n) {
+    int min = 1000, minIndex;
+    for(int i = 0; i < n; i++) {
+        if(distance[i] < min && visited[i] == 0) {
+            min = distance[i];
+            minIndex = i;
+        }
+    }
+    return minIndex;
+}
+//Hien thi duong di
+void disPlayPath(int distance[], int pred[], int n,int star) {
+    for(int i = 0; i < n; i++) {
+        if(i != star && distance[i] != 1000) {
+            printf("Distance from %d to %d:%d\n",star,i,distance[i]);
+            printf("Path:%d",i);
+            int j = i;
+            do {
+                j = pred[j];
+                printf("<-%d",j);
+            } while(j != star);
+        }
+    }
+}
+void dijkstra(matG g, int star) {
+    int distance[g.numvertices];
+    int visited[g.numvertices], pred[g.numvertices];
+    //Khoi tao khoang cach ban dau
+    for(int i = 0; i < g.numvertices; i++) {
+        distance[i] = g.graph[star][i];
+        visited[i] = 0;
+        pred[i] = star;
+    }
+    visited[star] = 1;
+    distance[star] = 0;
+    //Tnh duong di
+    for(int count = 0; count < g.numvertices - 1; count++) {
+        //Tim nut co khoang cach ngan nhat voi nut ban dau
+    int next = minDistance(distance, visited, g.numvertices);
+        //Cap nhat duong di
+        visited[next] = 1;
+        for(int i = 0; i < g.numvertices; i++) {
+            if(distance[next] + g.graph[next][i] < distance[i] && visited[i] == 0) {
+                distance[i] = distance[next] + g.graph[next][i];
+                pred[i] = next;
+            }
+        }
+    }
+    //Hien thi duong di
+    disPlayPath(distance, pred, g.numvertices, star);
 }
